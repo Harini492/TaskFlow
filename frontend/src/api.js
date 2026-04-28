81327bd581e10684c1d566cba7006032af73e7a4
Harini492 <harinirenu10@gmail.com>
2026-04-28 17:03:32 +0530

    deployment fixes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskflow-jjby.onrender.com",
 });
 
 // 🔐 Attach token properly (IMPORTANT FIX)
